53922417e9c932835451c64efced4627b424d581
Joap-Filho <fjoao3756@gmail.com>
2025-09-01 00:03:11 -0300

    -

--- src/main/java/com/sharedexpenses/app/service/ExpenseService.java
+++ src/main/java/com/sharedexpenses/app/service/ExpenseService.java
@@ -18,6 +18,7 @@ import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
